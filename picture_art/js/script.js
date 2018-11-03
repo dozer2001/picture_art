9d97f49e51361e3170a3a7d15b062e0cdb7c839c
Ilya Kucher <dozer2001@mail.ru>
2018-11-03 19:58:16 +0300

    'ModalWithInterval'

--- picture_art/js/script.js
+++ picture_art/js/script.js
@@ -248,3 +248,21 @@ wrapper.addEventListener('mouseout',function (event) {
     }
 });
 // mouseover End
+
+//ModalWithInterval start
+
+let popupConsultation = document.querySelector('.popup-consultation');
+setTimeout(modalTimeOut,6000);
+function modalTimeOut() {
+    let pup1 = document.querySelector('.popup-gift'),
+        pup2 = document.querySelector('.popup-design'),
+        pup3 = document.querySelector('.popup-consultation');
+
+    if(pup1.style.display !== 'block' && pup2.style.display !== 'block' && pup3.style.display !== 'block'){
+        popupConsultation.style.display = 'block';
+    }
+
+
+}
+
+//ModalWithInterval end
